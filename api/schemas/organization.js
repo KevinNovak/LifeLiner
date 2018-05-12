e4f3dd0faeaeebd1ac0f42de3ae9fc3b9cac89bb
Kevin Novak <kevinmnovak@yahoo.com>
2018-05-12 00:31:26 -0400

    Fix regex patterns. Additional validation.

--- api/schemas/organization.js
+++ api/schemas/organization.js
@@ -1,15 +1,19 @@
 const Schema = require('mongoose').Schema;
 const ContactsSchema = require('./contacts');
 const contactsValidator = require('../validators/contactsValidator');
+const regex = require('../data/regex');
 
 var OrganizationSchema = new Schema(
     {
         name: {
             type: String,
+            minlength: [2, 'Organization name is too short.'],
+            maxlength: [80, 'Organization name is too long.'],
             required: [true, 'Organization name is required.']
         },
         website: {
             type: String,
+            match: regex.url,
             required: [true, 'Organization website is required.']
         },
         contacts: {
--- api/schemas/text.js
+++ api/schemas/text.js
@@ -3,18 +3,20 @@ const audiences = require('../data/audiences.json');
 const regex = require('../data/regex');
 
 const numberPattern = new RegExp(
-    `(${regex.phone.longCode}|${regex.phone.shortCode})`
+    `(${regex.phone.shortCode.source}|${regex.phone.longCode.source})`
 );
 
 var TextSchema = new Schema(
     {
         number: {
             type: String,
-            match: [numberPattern, 'Phone number is not valid.'],
+            match: [numberPattern, 'Phone number to text is not valid.'],
             required: [true, 'Phone number to text is required.']
         },
         phrase: {
-            type: String
+            type: String,
+            minlength: [1, 'Text phrase is too short.'],
+            maxlength: [50, 'Text phrase is too long.']
         },
         audience: {
             type: [String],
