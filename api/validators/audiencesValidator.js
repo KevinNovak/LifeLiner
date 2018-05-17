a1e7a5777a3a913223c558e8ee39e8d3b6bc7ae7
Kevin Novak <kevinmnovak@yahoo.com>
2018-05-16 20:59:00 -0400

    audienceIdsExist return false on error

--- api/validators/audiencesValidator.js
+++ api/validators/audiencesValidator.js
@@ -1,13 +1,18 @@
 const AudienceModel = require('../models/audience');
 
 async function audienceIdsExist(audienceIds) {
-    for (var audienceId of audienceIds) {
-        var count = await AudienceModel.count({ _id: audienceId });
-        if (count < 1) {
-            return false;
+    try {
+        for (var audienceId of audienceIds) {
+            var count = await AudienceModel.count({ _id: audienceId });
+            if (count < 1) {
+                return false;
+            }
         }
+        return true;
+    } catch (error) {
+        console.error(error.message);
+        return false;
     }
-    return true;
 }
 
 module.exports = {
